77a5040148bb6aeb796d9b68383bee34838e3cb5
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-11-14 11:20:33 +0530

    Country district state Controller Done

--- ProBankCoreMVC/Controllers/StateMasterController.cs
+++ ProBankCoreMVC/Controllers/StateMasterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models;
 using ProBankCoreMVC.Interfaces;
 
 namespace ProBankCoreMVC.Controllers
@@ -14,19 +15,80 @@ namespace ProBankCoreMVC.Controllers
             _StateMaster = stateMaster;
         }
 
-        [HttpGet("GetStateById")]
-
-        public async Task<ActionResult> GetStateById(int stateCode)
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
         {
             try
             {
-                var States =await _StateMaster.GetStateById(stateCode);
+                var states = await _StateMaster.GetAll();
+                return Ok(states);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error while fetching states: {ex.Message}");
+            }
+        }
+
+        [HttpGet("GetStateById")]
+        public async Task<ActionResult> GetStateById(int stateCode, int countryCode)
+        {
+            try
+            {
+                var States = await _StateMaster.GetStateById(stateCode, countryCode);
                 return Ok(States);
             }
             catch (Exception ex)
             {
                 throw;
             }
+        }
+
+        [HttpPost("Save")]
+        public async Task<IActionResult> Save([FromBody] DTOStateMaster state)
+        {
+            if (state == null || string.IsNullOrWhiteSpace(state.Name))
+                return BadRequest("Country name cannot be empty.");
+
+            try
+            {
+                await _StateMaster.Save(state);
+                return Ok(new { message = "State saved successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error while saving State: {ex.Message}");
+            }
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Update([FromBody] DTOStateMaster state)
+        {
+            if (state == null || state.Code <= 0) return BadRequest("Invalid State ID.");
+
+            try
+            {
+                await _StateMaster.Update(state);
+                return Ok(new { message = "State updated successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error while updating State: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("Delete/{stateCode}")]
+        public async Task<IActionResult> Delete(long stateCode, int countryCode)
+        {
+            try
+            {
+                await _StateMaster.Delete(stateCode, countryCode);
+                return Ok(new { message = "State Deleted Successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error while deleting state:{ex.Message}");
+            }
+
         }
     }
 }
--- ProBankCoreMVC/Repositries/StateMasterRepository.cs
+++ ProBankCoreMVC/Repositries/StateMasterRepository.cs
@@ -13,23 +13,80 @@ namespace ProBankCoreMVC.Repositries
         {
             _dapperContext = dapperContext;
         }
+        public async Task<IEnumerable<DTOStateMaster>> GetAll()
+        {
+            const string query = @"
+                  select Code,Name,Country_Code from StateMast
+                ORDER BY Code DESC";
 
-        public async Task<DTOStateMaster?> GetStateById( int stateCode)
+            using (var conn = _dapperContext.CreateConnection())
+            {
+                return await conn.QueryAsync<DTOStateMaster>(query);
+            }
+        }
+
+        public async Task Save(DTOStateMaster State_List)
+        {
+            const string query = @"
+                INSERT StateMast(Code, Name,Country_Code)
+                VALUES (@Code, @Name,@Country_Code)";
+            var countryCode = State_List.Country_Code;
+            long newCode = await GenerateStateCode(countryCode);
+
+            using (var conn = _dapperContext.CreateConnection())
+            {
+                await conn.ExecuteAsync(query, new
+                {
+                    Code = newCode,
+                    Name = State_List.Name,
+                    Country_Code = State_List.Country_Code
+                });
+            }
+        }
+
+        public async Task Update(DTOStateMaster State_List)
+        {
+            const string query = @"
+                UPDATE StateMast
+                SET Name = @Name
+                WHERE Code = @Code";
+
+
+            using (var conn = _dapperContext.CreateConnection())
+            {
+                await conn.ExecuteAsync(query, new
+                {
+                    State_List.Name,
+                    State_List.Code
+                });
+            }
+        }
+
+        public async Task Delete(long stateCode, int countryCode)
+        {
+            const string query = @"DELETE FROM StateMast WHERE Code = @Code and Country_Code = @Country_Code";
+
+            using (var conn = _dapperContext.CreateConnection())
+            {
+                await conn.ExecuteAsync(query, new { Code = stateCode, Country_Code = countryCode });
+            }
+        }
+
+
+        public async Task<DTOStateMaster?> GetStateById(int stateCode, int countryCode)
         {
             var query = @"
-        SELECT
-            TRN_NO,STATE_NAME,COUNTRY_CODE
-        FROM StateMast
-        WHERE  TRN_NO = @StateCode";
+            select Code,Name,Country_Code from StateMast
+            WHERE  Code = @StateCode and Country_Code = @Country_Code";
 
             try
             {
                 using (var connection = _dapperContext.CreateConnection())
                 {
                     var result = await connection.QueryFirstOrDefaultAsync<DTOStateMaster>(query,
-                        new { StateCode = stateCode });
+                        new { StateCode = stateCode, Country_Code = countryCode });
 
-                    return result; 
+                    return result;
                 }
             }
             catch (Exception)
@@ -38,5 +95,23 @@ namespace ProBankCoreMVC.Repositries
             }
         }
 
+        private async Task<long> GenerateStateCode(int countryCode)
+        {
+            const string query = "SELECT TOP 1 Code FROM StateMast where Country_Code= @Country_Code ORDER BY Code DESC";
+
+            using (var conn = _dapperContext.CreateConnection())
+            {
+                var lastId = await conn.ExecuteScalarAsync<long?>(query, new { Country_Code = countryCode });
+                return (lastId ?? 0) + 1;
+            }
+        }
+
     }
+
+
+
+
+
+
+
 }
