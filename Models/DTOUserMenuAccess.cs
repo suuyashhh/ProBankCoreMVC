6a07f3cd3711b95c6017d718b027c2c32d815785
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-11-24 16:54:17 +0530

    user menu access api Done 1

---
+++ Models/DTOUserMenuAccess.cs
@@ -0,0 +1,29 @@
+﻿// Models/DTOMenuMasterItem.cs
+namespace Models
+{
+    public class DTOUserMenuAccess
+    {
+        public long UserGrad { get; set; }          // UserGrade.Code
+        public int ProgrameId { get; set; } = 1;    // Programe_ID (default 1 like old page)
+        public List<long> SelectedMenuIds { get; set; } = new();  // MenuIDs with Show_YN = 'Y'
+    }
+    public class DTOUserGrade
+    {
+        public long Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class DTOMenuMasterItem
+    {
+        public long MenuId { get; set; }
+        public string MenuName { get; set; }
+
+        public int ProgrameId { get; set; }
+        public long MainMenuId { get; set; }
+
+        public int SeqNo1 { get; set; }
+        public int SeqNo2 { get; set; }
+        public int SeqNo3 { get; set; }
+        public int SeqNo4 { get; set; }
+        public int SeqNo5 { get; set; }
+    }
+}
