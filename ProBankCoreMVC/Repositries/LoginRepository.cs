c2b879f3ab031cabe24d76a9a5401da2b29d063a
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-11-25 18:04:51 +0530

    In Login passed designation and userLevel

--- ProBankCoreMVC/Repositries/LoginRepository.cs
+++ ProBankCoreMVC/Repositries/LoginRepository.cs
@@ -25,14 +25,18 @@ namespace ProBankCoreMVC.Repositries
             {
                 var query = @"
                 SELECT TOP 1
-                    LOGIN_IP,
-                    NAME,
-                    WORKING_BRANCH,
-                    ALLOW_BR_SELECTION,
-                    ini AS INI,
-                    code AS CODE
-                FROM UserMast
-                WHERE ini = @INI AND code = @CODE
+                u.LOGIN_IP,
+	            u.user_level AS USER_LAVEL,
+                u.NAME,
+                u.WORKING_BRANCH,
+                u.ALLOW_BR_SELECTION,
+                u.ini AS INI,
+                u.code AS CODE,
+	            ug.name AS DESIGNATION
+                FROM UserMast AS u
+                JOIN UserGrade AS ug
+                ON ug.Code = u.user_level
+                WHERE u.ini = @INI AND u.code = @CODE
             ";
 
                 using (var connection = context.CreateConnection())
