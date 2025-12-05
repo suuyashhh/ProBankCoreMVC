cda8188959fabac9f058dec79c577e315a1d9f75
Atharv-Patil-8 <mail.to.atharv.8@gmail.com>
2025-12-05 13:47:12 +0530

    Comman Master

--- Models/DTOCommanMaster.cs
+++ Models/DTOCommanMaster.cs
@@ -8,5 +8,12 @@ namespace Models
 {
     public class DTOCommanMaster
     {
+        public int flag { get; set; }
+        public string TblName { get; set; }
+        public string fld1 { get; set; }
+        public string fld2 { get; set; }
+        //public string fld1val { get; set; }
+        public string fld2val { get; set; }
+        public string Msg { get; set; }
     }
 }
