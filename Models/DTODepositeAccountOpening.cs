154b9f8365b9deda960c0972e9c19e392c4fc44f
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-12-13 18:18:02 +0530

    Deposite Account Controller  done

---
+++ Models/DTODepositeAccountOpening.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DTODepositeAccountOpening
+    {
+        public string type { get; set; }
+        public int code { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ ProBankCoreMVC/Interfaces/IDepositeAccountOpening.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace ProBankCoreMVC.Interfaces
+{
+    public interface IDepositeAccountOpening
+    {
+        Task<IEnumerable<DTODepositeAccountOpening>> GetGlCodeAll();
+    }
+}
